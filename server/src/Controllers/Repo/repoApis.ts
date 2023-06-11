378251e1a617f72426e15b4096b375753d32fef3
omkar <saseomkar214@gmail.com>
2023-06-12 03:19:47 +0530

    file structure and file content routes

--- server/src/Controllers/Repo/repoApis.ts
+++ server/src/Controllers/Repo/repoApis.ts
@@ -1,4 +1,13 @@
 import axios from "axios";
+import { config } from "dotenv";
+
+config({ path: "../../.env" })
+interface FileStructure {
+    name: string;
+    type: "dir" | "file";
+    children?: FileStructure[];
+
+}
 
 export const fetchUserPublicRepos = async (username: string): Promise<{}[] | null> => {
     try {
@@ -59,3 +68,60 @@ export const fetchRepoLanguages = async (username: string, reponame: string): Pr
         return null;
     }
 }
+interface FileStructure {
+    name: string;
+    type: 'file' | 'dir';
+    children?: FileStructure[];
+}
+
+export const fetchRepoFilesStructure = async (username: string, reponame: string, path = ''): Promise<FileStructure[]> => {
+    try {
+        const response = await axios.get(`https://api.github.com/repos/${username}/${reponame}/contents/${path}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_PERSONAL_TOKEN}`
+            }
+        });
+        const contents = response.data;
+
+        const fileStructure: FileStructure[] = [];
+
+        for (const item of contents) {
+            if (item.type === 'dir') {
+                const nestedStructure = await fetchRepoFilesStructure(username, reponame, item.path);
+                fileStructure.push({
+                    name: item.name,
+                    type: 'dir',
+                    children: nestedStructure,
+                });
+            } else {
+                fileStructure.push({
+                    name: item.name,
+                    type: 'file',
+                });
+            }
+        }
+
+        return fileStructure;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+export const fetchFileContent = async (username: string, reponame: string, path: string): Promise<string> => {
+    try {
+        const response = await axios.get(`https://api.github.com/repos/${username}/${reponame}/contents/${path}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_PERSONAL_TOKEN}`
+            }
+        });
+
+        const fileContent = Buffer.from(response.data.content, 'base64').toString('utf-8');
+        console.log(typeof fileContent);
+
+        return fileContent;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
--- server/src/Routes/repoRoutes.ts
+++ server/src/Routes/repoRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { getUserPublicRepos, getUserStarredRepos, getSingleRepo, getRepoCommits, getRepoLanguages } from "../Controllers/Repo/repoController";
+import { getUserPublicRepos, getUserStarredRepos, getSingleRepo, getRepoCommits, getRepoLanguages, getRepoFilesStructure, getFileContent } from "../Controllers/Repo/repoController";
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.route("/single/:username/:repo").get(getSingleRepo);
 router.route("/starred/:username").get(getUserStarredRepos);
 router.route("/commits/:username/:reponame").get(getRepoCommits);
 router.route("/languages/:username/:reponame").get(getRepoLanguages);
+router.route("/file-structure").post(getRepoFilesStructure);
+router.route("/file-content").post(getFileContent);
 
 
 
